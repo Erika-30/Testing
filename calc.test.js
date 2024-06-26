a4148c078c5e91f5f740ba86abb8eccbd772190a
jhuisa <huisaj30@gmail.com>
2024-06-26 16:43:17 -0500

    testing divide and substract

--- calc.test.js
+++ calc.test.js
@@ -1,8 +1,12 @@
-describe("calc", () => {
+const calcHelpers = require("./clac");
+
+describe("calculator", () => {
   test("adds 1 + 2 to equal 3", () => {
-    expect(2 + 5).toBe(8);
+    expect(2 + 5).toBe(7);
   });
   describe("multiply by 3", () => {
+    let multiplyBy3 = calcHelpers.multiplyBy3;
+
     test("does exist the function?", () => {
       expect(multiplyBy3).toBeDefined();
     });
@@ -13,12 +17,30 @@ describe("calc", () => {
   });
 
   describe("addTheSe", () => {
+    let addTheSe = calcHelpers.addTheSe;
+
     test("does exist the function?", () => {
       expect(addTheSe).toBeDefined();
     });
 
     test("resive two numbes to add them", () => {
       expect(addTheSe(3, 5)).toEqual(8);
+    });
+  });
+
+  describe("divideBy2", () => {
+    let divideBy2 = calcHelpers.divideBy2;
+
+    test("resive a number to divide by 2", () => {
+      expect(divideBy2(10)).toBe(5);
+    });
+  });
+
+  describe("substract", () => {
+    let substract = calcHelpers.substract;
+
+    test("resive two numbers to substract second from first", () => {
+      expect(substract(20, 5)).toBe(15);
     });
   });
 });
--- clac.js
+++ clac.js
@@ -1,7 +1,16 @@
-function ultiplyByThree(num) {
+function multiplyBy3(num) {
   return num * 3;
 }
 
 function addTheSe(a, b) {
   return a + b;
 }
+
+function divideBy2(num) {
+  return num / 2;
+}
+function substract(a, b) {
+  return a - b;
+}
+
+module.exports = { multiplyBy3, addTheSe, divideBy2, substract };
